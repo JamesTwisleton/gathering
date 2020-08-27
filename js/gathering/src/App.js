689eaca3d94c1003a24a8ac5039af0df3caf691f
James Twisleton <ajtwisleton@connect.ust.hk>
2020-08-27 23:10:39 +0100

    removed pointless things

--- js/gathering/src/App.js
+++ js/gathering/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Canvas } from './Canvas';
-
 function App() {
   let socket = new WebSocket('ws://localhost:42069');
-  let payload = {user: "me", message: "hello sockets"};
   socket.onopen = () => {
-    // socket.send('Client is here!');
     socket.send('{"id": "move","message":"RIGHT"}');
-    // socket.send('{"id": "move","message":"UP"}');
   }
   socket.onmessage = (message) => {
     let parsed = JSON.parse(message.data);
-    // handle message from backend
   }
   return Canvas();
 }
