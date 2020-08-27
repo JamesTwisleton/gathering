3523e259a3848073e50ab610940ad19da3b9fc7f
Felix Bowman <felixgbowman@gmail.com>
2020-08-27 22:59:43 +0100

    Draw a purple grid

--- js/gathering/src/App.js
+++ js/gathering/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Canvas } from './Canvas';
+
 function App() {
   let socket = new WebSocket('ws://localhost:42069');
   let payload = {user: "me", message: "hello sockets"};
@@ -6,30 +9,10 @@ function App() {
     socket.send('{"id": "move","message":{"direction":"left"}}');
   }
   socket.onmessage = (message) => {
-    console.log(message);
     let parsed = JSON.parse(message.data);
-    console.log('message received', parsed);
     // handle message from backend
-}
-  return 'client is running';
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+  }
+  return Canvas();
 }
 
 export default App;
