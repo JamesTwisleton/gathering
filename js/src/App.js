2a9d4322e912950ce2790f169230b0e790fdbc71
Twisleton <ajtwisleton@gmail.com>
2020-08-29 13:28:08 +0100

    drawing player, update on refresh

--- js/src/App.js
+++ js/src/App.js
@@ -1,18 +1,7 @@
 import { Canvas } from './Canvas';
-import { WorldBuilder } from './services/WorldBuilder';
 function App() {
   let socket = new WebSocket('ws://localhost:42069');
-  let world;
-  socket.onopen = () => {
-    //socket.send('{"id": "move","message":"RIGHT"}');
-  }
-  socket.onmessage = (message) => {
-    let parsed = JSON.parse(message.data);
-    if(parsed.id === 'world') {
-      world = WorldBuilder(parsed);
-    }
-  }
-  return Canvas();
+  return Canvas(socket);
 }
 
 export default App;
