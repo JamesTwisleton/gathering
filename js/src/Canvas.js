e12dc5af8bc3b2e953cb1952c0619974428314f0
James Twisleton <ajtwisleton@connect.ust.hk>
2020-08-30 19:35:15 +0100

    default layout

--- js/src/Canvas.js
+++ js/src/Canvas.js
@@ -1,107 +1,114 @@
 import React from 'react';
 import { WorldBuilder } from './services/WorldBuilder';
-import GlobalStyles from "./styles/global";
+import GlobalStyles from './styles/global';
+import DefaultLayout from './pages/layouts/default';
 
 export function Canvas(socket) {
-  let world;
+    let world;
 
-  const canvasRef = React.createRef();
-  // const playerCanvasRef = React.createRef();
+    const canvasRef = React.createRef();
 
-  const resized = () => {
-    canvasRef.current.width = window.innerWidth;
-    canvasRef.current.height = window.innerHeight;
-    drawGrid(canvasRef.current);
-  };
+    const resized = () => {
+        canvasRef.current.width = window.innerWidth;
+        canvasRef.current.height = window.innerHeight;
+        drawGrid(canvasRef.current);
+    };
 
-  const handleMessage = (message) =>  {
-    let parsed = JSON.parse(message.data);
-    if(parsed.id === 'world') {
-      world = WorldBuilder(parsed);
-      drawUsers(canvasRef.current, world);
-    }
-  }
+    const handleMessage = (message) => {
+        let parsed = JSON.parse(message.data);
+        if (parsed.id === 'world') {
+            world = WorldBuilder(parsed);
+            drawUsers(canvasRef.current, world);
+        }
+    };
 
-  React.useEffect(() => {
-    resized();
-    window.addEventListener("resize", resized);
-    socket.onopen = () => {
-      socket.send('{"id": "move","message":"RIGHT"}');
-    }
-    socket.onmessage = (message) => {
-      handleMessage(message);
-    }
-  });
+    React.useEffect(() => {
+        resized();
+        window.addEventListener('resize', resized);
+        socket.onopen = () => {
+            socket.send('{"id": "move","message":"RIGHT"}');
+        };
+        socket.onmessage = (message) => {
+            handleMessage(message);
+        };
+    });
 
-  return <div id="wrapper">
-    <canvas ref={canvasRef}></canvas>
-    <GlobalStyles/>
-    </div>
+    return (
+        <DefaultLayout>
+        <GlobalStyles />
+            <div id="wrapper">
+                <canvas ref={canvasRef}></canvas>
+            </div>
+        </DefaultLayout>
+    );
 }
 
 function drawGrid(canvas) {
-  const cellRows = 10;
-  const ctx = canvas.getContext('2d');
+    const cellRows = 10;
+    const ctx = canvas.getContext('2d');
 
-  ctx.beginPath();
-  ctx.strokeStyle = '#FF00FF';
-  const incX = canvas.width / cellRows;
-  for (let i = 0; i <= canvas.width; i += incX) {
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, canvas.height);
-    ctx.stroke();
-  }
-  const incY = canvas.height / cellRows;
-  for (let i = 0; i <= canvas.height; i += incY) {
-    ctx.moveTo(0, i);
-    ctx.lineTo(canvas.width, i);
-    ctx.stroke();
-  }
-  ctx.closePath();
-  console.log("done");
+    ctx.beginPath();
+    ctx.strokeStyle = '#FF00FF';
+    const incX = canvas.width / cellRows;
+    for (let i = 0; i <= canvas.width; i += incX) {
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, canvas.height);
+        ctx.stroke();
+    }
+    const incY = canvas.height / cellRows;
+    for (let i = 0; i <= canvas.height; i += incY) {
+        ctx.moveTo(0, i);
+        ctx.lineTo(canvas.width, i);
+        ctx.stroke();
+    }
+    ctx.closePath();
+    console.log('done');
 }
 
 function drawUsers(canvas, world) {
-  const ctx = canvas.getContext('2d');
-  // ctx.moveTo(100, 0);
-  //   ctx.lineTo(100, canvas.height);
-  //   ctx.stroke();
-  for(var user in world.users) {
-    drawUser(ctx, world, user);
-  }
-  // console.log(world.users.[0]);
+    const ctx = canvas.getContext('2d');
+    // ctx.moveTo(100, 0);
+    //   ctx.lineTo(100, canvas.height);
+    //   ctx.stroke();
+    for (var user in world.users) {
+        drawUser(ctx, world, user);
+    }
+    // console.log(world.users.[0]);
 
-  // const cellRows = 10;
-  
+    // const cellRows = 10;
 
-  
-  // const incX = canvas.width / cellRows;
-  // for (let i = 0; i <= canvas.width; i += incX) {
-  //   ctx.moveTo(i, 0);
-  //   ctx.lineTo(i, canvas.height);
-  //   ctx.stroke();
-  // }
-  // const incY = canvas.height / cellRows;
-  // for (let i = 0; i <= canvas.height; i += incY) {
-  //   ctx.moveTo(0, i);
-  //   ctx.lineTo(canvas.width, i);
-  //   ctx.stroke();
-  // }
-  console.log("finished drawing users");
+    // const incX = canvas.width / cellRows;
+    // for (let i = 0; i <= canvas.width; i += incX) {
+    //   ctx.moveTo(i, 0);
+    //   ctx.lineTo(i, canvas.height);
+    //   ctx.stroke();
+    // }
+    // const incY = canvas.height / cellRows;
+    // for (let i = 0; i <= canvas.height; i += incY) {
+    //   ctx.moveTo(0, i);
+    //   ctx.lineTo(canvas.width, i);
+    //   ctx.stroke();
+    // }
+    console.log('finished drawing users');
 }
 
 function drawUser(ctx, world, user) {
-  // todo: move this to a better location
-  // left this as a variable so it could
-  // easily be changed in the future
-  const playerSize = 20;
+    // todo: move this to a better location
+    // left this as a variable so it could
+    // easily be changed in the future
+    const playerSize = 20;
 
-  ctx.beginPath();
-  let playerXPosition = (world.users[user].position.x / world.maxX) * window.innerWidth;
-  let playerYPosition = (world.users[user].position.y / world.maxY) * window.innerHeight;
-  ctx.fillStyle = world.users[user].color;
-  console.log(playerYPosition);
-  ctx.fillRect(playerXPosition - (playerSize / 2), playerYPosition - (playerSize / 2), playerSize, playerSize);
-  ctx.stroke();
-  ctx.closePath();
+    ctx.beginPath();
+    let playerXPosition = (world.users[user].position.x / world.maxX) * window.innerWidth;
+    let playerYPosition = (world.users[user].position.y / world.maxY) * window.innerHeight;
+    ctx.fillStyle = world.users[user].color;
+    console.log(playerYPosition);
+    ctx.fillRect(
+        playerXPosition - playerSize / 2,
+        playerYPosition - playerSize / 2,
+        playerSize,
+        playerSize
+    );
+    ctx.stroke();
+    ctx.closePath();
 }
