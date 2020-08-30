2ac11b52f41f68926aa0c7996d9b055e3b9bd4d2
JamesTwisleton <JamesTwisleton@users.noreply.github.com>
2020-08-30 20:30:47 +0000

    deploy: 10d8b658c5ca386dd891b0187d5052af8d7888e9

---
+++ precache-manifest.5205db72eb0dc150c2d30699f79208a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6d95ce20d16c8baa6ab2b04b987080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c6987a2d075672a0aff",
+    "url": "/static/css/main.1b4bc23d.chunk.css"
+  },
+  {
+    "revision": "127f44cb2aeb26c09ce8",
+    "url": "/static/js/2.9501c3e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9501c3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6987a2d075672a0aff",
+    "url": "/static/js/main.186dd0ab.chunk.js"
+  },
+  {
+    "revision": "96061f636e4f296626dc",
+    "url": "/static/js/runtime-main.e1a87c00.js"
+  }
+]);
