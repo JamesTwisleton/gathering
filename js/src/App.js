32833691398fc34270800e3152f84acad6468b5e
Twisleton <ajtwisleton@gmail.com>
2020-08-31 12:57:57 +0100

    started config

--- js/src/App.js
+++ js/src/App.js
@@ -1,6 +1,11 @@
 import { Canvas } from './Canvas';
+import config from './config';
 function App() {
-  let socket = new WebSocket('ws://localhost:42069');
+  let socketAddress = 'ws://localhost:42069';
+  if(config.mode === 'dev') {
+    socketAddress = `ws://${config.devServerAddress}:42069`;
+  }
+  let socket = new WebSocket(socketAddress);
   return Canvas(socket);
 }
 
