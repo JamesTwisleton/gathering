82aca8a3344a68a0a17e7a6c474d147d301fa539
James Twisleton <ajtwisleton@connect.ust.hk>
2020-09-02 14:18:07 +0100

    even better?

--- java/deployJavaApp.sh
+++ java/deployJavaApp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pkill -f .*javaApp
-cd java
 git pull -q -f
 mvn clean package
 screen -S javaApp -d -m java -jar --enable-preview target/gathering-0.0.1-SNAPSHOT.jar
--- js/deployReactApp.sh
+++ js/deployReactApp.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pkill -f .*reactApp
-cd js
 git pull -q -f
 npm-install-changed >/dev/null
 screen -S reactApp -d -m npm start
