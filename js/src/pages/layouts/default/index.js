f7e07289bd968fcf31a097ca120286e50b6ebb1d
James Twisleton <ajtwisleton@connect.ust.hk>
2020-09-05 12:55:44 +0100

    layouts

--- js/src/pages/layouts/default/index.js
+++ js/src/pages/layouts/default/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Row from 'react-bootstrap/Row';
 // import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
+import { NavigationBar } from '../../../components/NavigationBar';
+
 export default function DefaultLayout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+    return (
+        <>
+            <NavigationBar />
+            <Wrapper>{children}</Wrapper>
+        </>
+    );
 }
 DefaultLayout.propTypes = {
-  // children: PropTypes.element.isRequired,
+    // children: PropTypes.element.isRequired,
 };
